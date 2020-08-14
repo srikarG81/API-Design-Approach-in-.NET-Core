65cb792132a225cc0202a283c82befd8a86400b4
Srikar Gandhi <38348498+srikarG81@users.noreply.github.com>
2020-08-14 11:39:04 +0100

    add files

---
+++ Bookstore/Controllers/BookshelvesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bookstore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Bookstore.Controllers
+{
+    [ApiController]
+    [Route("/books/v1/users/{userId:alpha}/[controller]")]
+    public class BookshelvesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get(string userId)
+        {
+            if (string.IsNullOrEmpty(userId))// Mock user validation failed.
+            {
+                Error error = new Error() { Code = "400", Message = "Invalid user name or user does not exists." };
+                return BadRequest(new List<Error>() { error });
+            }
+
+            List<Bookshelves> bookshelves = new List<Bookshelves>();
+            bookshelves.Add(new Bookshelves()
+            {
+                Id = 1,
+                Name = "Have to Read"
+            });
+
+            bookshelves.Add(new Bookshelves()
+            {
+                Id = 2,
+                Name = "Favourite books"
+            });
+
+            return Ok(bookshelves);
+        }
+    }
+}
---
+++ Bookstore/Models/Bookshelves.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Bookstore
+{
+    public class Bookshelves
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
